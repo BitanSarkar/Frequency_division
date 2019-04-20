1b84e3403b874ca84b97b7d66ea4047c5a79f9b5
876arham <35299150+876arham@users.noreply.github.com>
2019-04-20 18:59:52 +0530

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,8 @@
 clc;clear all; close all;
-input = get_input();
-channel = transmission(input);
+input1 = get_input();
+input2 = get_input();
+input3 = get_input();
+input4 = get_input();
+channel = transmission(input1,input2, input3,input4);
+L = length(input1);
+output= get_output(channel,L);
